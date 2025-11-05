98967f96fdea06019437c12f7b2da66bb19db352
vaidik <vaidik.mobileappdev@gmail.com>
2025-11-05 15:18:10 +0530

    1) config port change.

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ import config from './src/config/config.js';
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 
-app.listen(3001, () => {
+app.listen(config.port, () => {
   console.log(`Server is running on port ${3001}`);
 });
